16720e5dd968dcff5ae58197daeb5022fd271f01
Alireza <ar.mahdavi21@gmail.com>
2025-01-03 01:37:49 -0500

    Add files via upload

---
+++ e2e_loan_approval_predictor/powershell.ps1
@@ -0,0 +1,20 @@
+﻿cd code
+mkdir e2e_loan_approval_predictor
+python --version
+
+python -m venv venv
+
+Get-ExecutionPolicy
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+.\venv\Scripts\Activate.ps1
+
+pip freeze > requirements.txt
+
+mkdir app
+
+pip install fastapi uvicorn
+pip install pandas joblib sk-scikit-learn
+pip list
+
+uvicorn app.main:app --reload
